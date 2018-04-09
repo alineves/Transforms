99fb57886fb1c146033af37fd4efed916f2f63d8
Aline Neves Normanton <alineves@gmail.com>
2018-04-09 16:38:32 -0300

    Novos algoritmos

--- dwt/encoded.py
+++ dwt/encoded.py
@@ -17,6 +17,72 @@ class Mode(Enum):
     DB6 = 6
     DB7 = 7
     DB8 = 8
+    DB9 = 9
+    DB10 = 10
+    DB11 = 11
+    DB12 = 12
+    DB13 = 13
+    DB14 = 14
+    DB15 = 15
+    DB16 = 16
+    DB17 = 17
+    DB18 = 18
+    DB19 = 19
+    DB20 = 20
+    DB21 = 21
+    DB22 = 22
+    DB23 = 23
+    DB24 = 24
+    DB25 = 25
+    DB26 = 26
+    DB27 = 27
+    DB28 = 28
+    DB29 = 29
+    DB30 = 30
+    DB31 = 31
+    DB32 = 32
+    DB33 = 33
+    DB34 = 34
+    DB35 = 35
+    DB36 = 36
+    DB37 = 37
+    DB38 = 38
+    SYM2 = 39
+    SYM3 = 40
+    SYM4 = 41
+    SYM5 = 42
+    SYM6 = 43
+    SYM7 = 44
+    SYM8 = 45
+    SYM9 = 46
+    SYM10 = 47
+    SYM11 = 48
+    SYM12 = 49
+    SYM13 = 50
+    SYM14 = 51
+    SYM15 = 52
+    SYM16 = 53
+    SYM17 = 54
+    SYM18 = 55
+    SYM19 = 56
+    SYM20 = 57
+    COIF1 = 58
+    COIF2 = 59
+    COIF3 = 60
+    COIF4 = 61
+    COIF5 = 62
+    COIF6 = 63
+    COIF7 = 64
+    COIF8 = 65
+    COIF9 = 66
+    COIF10 = 67
+    COIF11 = 68
+    COIF12 = 69
+    COIF13 = 70
+    COIF14 = 71
+    COIF15 = 72
+    COIF16 = 73
+    COIF17 = 74
 
     @classmethod
     def fromString(cls, string):
@@ -28,7 +94,73 @@ class Mode(Enum):
             'db5': cls.DB5,
             'db6': cls.DB6,
             'db7': cls.DB7,
-            'db8': cls.DB8
+            'db8': cls.DB8,
+            'db9': cls.DB9,
+            'db10': cls.DB10,
+            'db11': cls.DB11,
+            'db12': cls.DB12,
+            'db13': cls.DB13,
+            'db14': cls.DB14,
+            'db15': cls.DB15,
+            'db16': cls.DB16,
+            'db17': cls.DB17,
+            'db18': cls.DB18,
+            'db19': cls.DB19,
+            'db20': cls.DB20,
+            'db21': cls.DB21,
+            'db22': cls.DB22,
+            'db23': cls.DB23,
+            'db24': cls.DB24,
+            'db25': cls.DB25,
+            'db26': cls.DB26,
+            'db27': cls.DB27,
+            'db28': cls.DB28,
+            'db29': cls.DB29,
+            'db30': cls.DB30,
+            'db31': cls.DB31,
+            'db32': cls.DB32,
+            'db33': cls.DB33,
+            'db34': cls.DB34,
+            'db35': cls.DB35,
+            'db36': cls.DB36,
+            'db37': cls.DB37,
+            'db38': cls.DB38,
+            'sym2': cls.SYM2,
+            'sym3': cls.SYM3,
+            'sym4': cls.SYM4,
+            'sym5': cls.SYM5,
+            'sym6': cls.SYM6,
+            'sym7': cls.SYM7,
+            'sym8': cls.SYM8,
+            'sym9': cls.SYM9,
+            'sym10': cls.SYM10,
+            'sym11': cls.SYM11,
+            'sym12': cls.SYM12,
+            'sym13': cls.SYM13,
+            'sym14': cls.SYM14,
+            'sym15': cls.SYM15,
+            'sym16': cls.SYM16,
+            'sym17': cls.SYM17,
+            'sym18': cls.SYM18,
+            'sym19': cls.SYM19,
+            'sym20': cls.SYM20,
+            'coif1': cls.COIF1,
+            'coif2': cls.COIF2,
+            'coif3': cls.COIF3,
+            'coif4': cls.COIF4,
+            'coif5': cls.COIF5,
+            'coif6': cls.COIF6,
+            'coif7': cls.COIF7,
+            'coif8': cls.COIF8,
+            'coif9': cls.COIF9,
+            'coif10': cls.COIF10,
+            'coif11': cls.COIF11,
+            'coif12': cls.COIF12,
+            'coif13': cls.COIF13,
+            'coif14': cls.COIF14,
+            'coif15': cls.COIF15,
+            'coif16': cls.COIF16,
+            'coif17': cls.COIF17
         }[string]
 
     def toString(self):
