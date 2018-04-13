f7cf42ae4a9a3da548695066b9f1ffc16bd15d3b
Aline Neves <alineves@gmail.com>
2018-04-12 22:52:56 -0300

    incluir marcacao de tempo de encode e decode

--- teste1.py
+++ teste1.py
@@ -6,15 +6,25 @@ import dcts.dct1 as dct1
 import dcts.dct2 as dct2
 import dcts.dct3 as dct3
 import dcts.dct4 as dct4
+import time
+
+def current_milli_time():
+    return int(round(time.time() * 1000))
 
 fs, audData = wv.open_wave("./waves/f0001038.16k.WAV")
 
+b = current_milli_time()
 encoded = codec.encode(audData, fs, 0.02, dct2, 20)
+a = current_milli_time()
+print('Tempo de encode: ', a - b)
 
 encoded.descartar(160)
 encoded.saveToFile("./result/f0001038.16k.dct4")
 
+b = current_milli_time()
 decoded = codec.decodeFromEncoded(encoded, dct4)
+a = current_milli_time()
+print('Tempo de decode: ', a - b)
 wv.save_wave("./result/f0001038.16k-f1dct4.desc160.s20.wav", fs, decoded, 16)
 
 newEncoded = enc.WaveEncoded.loadFromFile("./result/f0001038.16k.dct4")
--- teste_dwt._sorted_energy.py
+++ teste_dwt._sorted_energy.py
@@ -7,14 +7,14 @@ import time
 def current_milli_time():
     return int(round(time.time() * 1000))
 
-fs, audData = wv.open_wave("./waves/f0001038.16k.WAV")
+fs, audData = wv.open_wave("./waves/f0001038.8k.WAV")
 
 b = current_milli_time()
-encoded = codec.encode(audData, fs, 0.1, 'db8', 6, sobreposicao=0)
+encoded = codec.encode(audData, fs, 2, 'db12', 9, sobreposicao=0)
 a = current_milli_time()
 print('Tempo de encode: ', a - b)
-
-encoded.setPorcentagemDescarte(0.8)
+print("quantidadeQuadros", encoded.quantidadeQuadros())
+encoded.setPorcentagemDescarte(0.85)
 
 b = current_milli_time()
 rest = codec.decode(encoded)
@@ -23,7 +23,7 @@ print('Tempo de decode: ', a - b)
 
 encoded.saveToFile('./result/f0001038.dwt')
 
-wv.save_wave("./result/f0001038.16k.db8.dec6.removeCd1.s0.wav", fs, rest, 16)
+wv.save_wave("./result/f0001038.8k.db12.dec9.remove85percent.s0.wav", fs, rest, 16)
 
 newEncoded = enc.WaveEncoded.fromFile('./result/f0001038.dwt')
 # rest = codec.decode(newEncoded)
